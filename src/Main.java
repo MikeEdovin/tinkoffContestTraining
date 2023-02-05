a55492788b78529b31505bb237babc1369fa8013
MikeEdovin <edovinmihail@gmail.com>
2023-02-05 09:02:31 +0200

    byte

--- src/Main.java
+++ src/Main.java
@@ -9,35 +9,29 @@ public class Main {
     }
 
 public static class Input {
-    private int nrOfEmployes;
-    private int time;
-    private int[] floors;
-    private int timePerson;
-    private int result;
+    private byte nrOfEmployes;
+    private byte time;
+    private byte[] floors;
+    private byte timePerson;
+    private short result;
     public void getInput() {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             String string = br.readLine();
             String[] first = string.split("\\s+");
-            nrOfEmployes = Integer.parseInt(first[0]);
-            time = Integer.parseInt(first[1]);
-            floors = new int[nrOfEmployes];
-            if(nrOfEmployes<2||nrOfEmployes>100||time<2||time>100){
-                System.exit(0);
-            }
+            nrOfEmployes = Byte.parseByte(first[0]);
+            time = Byte.parseByte(first[1]);
+            floors = new byte[nrOfEmployes];
 
             String secondString = br.readLine();
             String[] second = secondString.split("\\s+");
             for (int i = 0; i < second.length; i++) {
-                int floor=Integer.parseInt(second[i]);
-                if(floor>100){
-                    System.exit(0);
-                }
+                byte floor=Byte.parseByte(second[i]);
                 floors[i] = floor;
 
             }
             String third = br.readLine();
-            timePerson = Integer.parseInt(third)-1;
+            timePerson = (byte) (Byte.parseByte(third)-1);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -45,11 +39,11 @@ public static class Input {
     }
     public void calculate() {
         if(time>floors[timePerson]-floors[0]||time>floors[nrOfEmployes-1]-floors[timePerson]){
-            result= floors[nrOfEmployes-1]-floors[0];
+            result= (short) (floors[nrOfEmployes-1]-floors[0]);
         }else{
             int left=floors[timePerson]-floors[0];
             int right=floors[nrOfEmployes-1]-floors[timePerson];
-            result=Math.min(left,right)*2+Math.max(left,right);
+            result= (short) (Math.min(left,right)*2+Math.max(left,right));
         }
     }
     public void display(){
